884cc71390b2c6d3e2e848409d5916fac9e61550
Grammar Grurus <truonglvhe176355@fpt.edu.vn>
2025-04-22 19:10:44 +0700

    Complete flow cart, and Stripe payment

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const usersRouter = require("./routers/usersRouter");
 const userModel = require("./models/user.model");
+const lessonRouter = require("./routers/lessonRouter");
+const courseRouter = require("./routers/courseRouter");
+const cartRouter = require("./routers/cartRouter");
+const enrollmentRouter = require("./routers/enrollmentRouter");
+const paymentRouter = require("./routers/paymentRouter");
 dotenv.config();
 connectDB();
 
 const app = express();
 // Xử lý CORS
 app.use(cors());
-
+app.post("/api/payments/webhook", express.raw({ type: "application/json" }), paymentRouter);
 // Xử lý JSON và form-data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,6 +35,11 @@ app.use("/api/all",async (req, res) => {
         res.status(500).json({ message: "Lỗi server, vui lòng thử lại!" });
       }
 })
+app.use("/api/lessons", lessonRouter);
+app.use("/api/courses", courseRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/enrollments", enrollmentRouter);
+app.use("/api/payments", paymentRouter);
 const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => {
   console.log(`🚀 Server chạy tại http://localhost:${PORT}`);
