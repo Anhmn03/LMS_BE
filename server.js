2696d070f334f8ae08240545f0a442cb052917c3
Grammar Grurus <truonglvhe176355@fpt.edu.vn>
2025-04-24 20:49:06 +0700

    fix update status lesson

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use("/api/coursesManager", courseManagerRouter);
 app.use("/api/courses", courseRoutes);
 app.use("/api/auth", authRouter);
 app.use("/api/lessons", lessonRouter);
-app.use("/api/courses", courseRouter);
+app.use("/api/courses1", courseRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/enrollments", enrollmentRouter);
 app.use("/api/payments", paymentRouter);
